b1e8157ffc007f26394d30b39ff95ef57a75661d
fabinjoe <66658906+fabinjoe@users.noreply.github.com>
2021-01-08 12:03:54 -0500

    Add Code
    In this commit, I added the code for this project

---
+++ periodicTableWebApp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for periodicTableWebApp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'periodicTableWebApp.settings')
+
+application = get_wsgi_application()
